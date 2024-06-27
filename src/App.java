b892e18c1bcf233ca3d523277be4b5a71a886192
Adriano <adriano.me283@gmail.com>
2024-06-26 21:47:16 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+   public static void main(String[] args) {
+    String nome;
+    int age;
+   }
+   public String nome;
+   public class App2{
+   public static void main(String[] args) throws Exception {
+   App app2 = new App();
+   app2.nome = "Adriano";
+
+   System.out.println(app2);
+   }
+   }
+}
